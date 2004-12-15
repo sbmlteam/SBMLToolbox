fc1abad3cdde0ae386da29ea3f69199f4d219c75
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2004-12-15 15:16:55 +0000

    caught bug with minus signs

--- trunk/SBMLToolbox/src/AccessModel/GetRateLawsFromReactions.m
+++ trunk/SBMLToolbox/src/AccessModel/GetRateLawsFromReactions.m
@@ -74,7 +74,7 @@ NumReactions = length(SBMLModel.reaction);
 
 for i = 1:NumberSpecies
     output = '';
- 
+
     % if species is a boundary condition (or constant in level 2
     % no rate law is required
     boundary = SBMLModel.species(i).boundaryCondition;
@@ -83,96 +83,97 @@ for i = 1:NumberSpecies
     else
         constant = -1;
     end;
-    
+
     if (boundary == 1)
         output = '0';
     elseif (constant ==1)
         output = '0';
     else
-        
-        %determine which reactions it occurs within 
+
+        %determine which reactions it occurs within
         for j = 1:NumReactions
-            
+
             SpeciesType = IsSpeciesInReaction(SBMLModel.species(i), SBMLModel.reaction(j));
-            
-            % record numbers of occurences of species as reactant/product 
+
+            % record numbers of occurences of species as reactant/product
             % and check that we can deal with reaction
             if (SpeciesType(1)>0)
-                
+
                 NoModifiers = length(SpeciesType);
                 NoReactants = SpeciesType(2);
                 NoProducts =  SpeciesType(3+NoReactants);
                 SavedNoReactants = SpeciesType(2);
-                
+
                 %--------------------------------------------------------------
                 % check that a species does not occur twice on one side of the
                 % reaction
                 if (NoReactants > 1 || NoProducts > 1)
                     error('GetRateLawsFromReactions(SBMLModel)\n%s', 'SPECIES OCCURS MORE THAN ONCE ON ONE SIDE OF REACTION');
                 end;
-                
+
                 %--------------------------------------------------------------
                 % check that reaction has a kinetic law formula
                 if (isempty(SBMLModel.reaction(j).kineticLaw))
                     error('GetRateLawsFromReactions(SBMLModel)\n%s', 'NO KINETC LAW SUPPLIED');
                 end;
                 %--------------------------------------------------------------
-                
-                
+
+
             end;
-            
+
             % species has been found in this reaction
-            while (SpeciesType(1) > 0) % 
-                
+            while (SpeciesType(1) > 0) %
+
                 % add the kinetic law to the output for this species
-                
-                if(NoProducts > 0) 
-                    
+
+                if(NoProducts > 0)
+
                     % Deal with case where parameter is defined within the reaction
                     % and thus the reaction name has been appended to the parameter
                     % name in the list in case of repeated use of same name
                     Param_Name = GetParameterFromReaction(SBMLModel.reaction(j));
-                    
-                    formula = SBMLModel.reaction(j).kineticLaw.formula;
-                    
+
+
                     if (~isempty(Param_Name))
                         ReviseParam_Name = GetParameterFromReactionUnique(SBMLModel.reaction(j));
-                        formula = Substitute(Param_Name, ReviseParam_Name, formula);
+                        formula = Substitute(Param_Name, ReviseParam_Name, SBMLModel.reaction(j).kineticLaw.formula);
+                    else
+                        formula = SBMLModel.reaction(j).kineticLaw.formula;
+
                     end;
-                    
-                    
-                    
+
+
+
                     % if stoichiometry = 1 no need to include it in formula
                     if (SpeciesType(3+SavedNoReactants+1) == 1)
-                        output = sprintf('%s + %s', output, formula);
+                        output = sprintf('%s + (%s)', output, formula);
                     else
-                        output = sprintf('%s + %u * %s', output, SpeciesType(3+SavedNoReactants+1), formula);
+                        output = sprintf('%s + %u * (%s)', output, SpeciesType(3+SavedNoReactants+1), formula);
                     end;
-                    
+
                     NoProducts = NoProducts - 1;
-                    
-                elseif (NoReactants > 0) 
-                    % DEAL WITH CASE WHERE KINETIC LAW LREADY HAS A MINUS SIGN    
-                    % since any kinetic law here will be subtracted any
-                    % existing minus signs must be replaced with plus
-                    formula = regexprep(SBMLModel.reaction(j).kineticLaw.formula, '-', '+');
-                    
+
+                elseif (NoReactants > 0)
+
                     % Deal with case where parameter is defined within the reaction
                     % and thus the reaction name has been appended to the parameter
                     % name in the list in case of repeated use of same name
                     Param_Name = GetParameterFromReaction(SBMLModel.reaction(j));
-                    
+
                     if (~isempty(Param_Name))
                         ReviseParam_Name = GetParameterFromReactionUnique(SBMLModel.reaction(j));
-                        formula = Substitute(Param_Name, ReviseParam_Name, formula);
+                        formula = Substitute(Param_Name, ReviseParam_Name, SBMLModel.reaction(j).kineticLaw.formula);
+                    else
+                        formula = SBMLModel.reaction(j).kineticLaw.formula;
+
                     end;
-                    
-                    
+
+
                     % if stoichiometry = 1 no need to include it in formula
                     if (SpeciesType(3) == 1)
-                        output = sprintf('%s - %s', output, formula);
+                        output = sprintf('%s - (%s)', output, formula);
                     else
-                        output = sprintf('%s - %u * %s', output, SpeciesType(3), formula);
+                        output = sprintf('%s - %u * (%s)', output, SpeciesType(3), formula);
                     end;
                     
                     NoReactants = NoReactants - 1;
@@ -212,7 +213,7 @@ NumberParams = length(InitialCharArray);
 
 
 % want these in order of shortest to longest
-% since shorter my be subsets of longer 
+% since shorter may be subsets of longer 
 % ie.  'alpha'  is a subset of 'alpha1'
 
 % determine length of each parameter
@@ -229,99 +230,9 @@ for i = 1:NumberParams
     OrderedReplacements{i} = ReplacementParams{Index(i)};
 end;
 
-% determine that occurences of each Initial parameter in the formula
-for i = 1:NumberParams
-    Hits{i} = strfind(Formula, OrderedCharArray{i});
-end;
-
-
-for i = 1:NumberParams
-    
-    for j = 1:length(Hits{i})
-        
-        % check whether a member of later arrays
-        % if so it is a subset ignore
-        for k = i+1:NumberParams
-            if (ismember(Hits{i}(j), Hits{k}))
-                Hits{i}(j) = 0;
-            else 
-                for s = 1:length(Hits{k})
-                    if ((Hits{i}(j) > Hits{k}(s)) && (Hits{i}(j) < Hits{k}(s) + NoCharsInParam(k)))
-                        Hits{i}(j) = 0;
-                    end;
-                end;
-            end;
-        end;
-    end;
-end;
-
-% determine the number of occurences of each parameter
-
- for i = 1:NumberParams
-     Number(i) = length(find(Hits{i}~=0));
- end;
- 
-% create an index of the starting points
-Index = {};
-for i = 1:NumberParams
-    
-    TempIndex = [];
-    for k = 1:length(Hits{i})
-        if (Hits{i}(k) ~= 0)
-            TempIndex = [TempIndex Hits{i}(k)];
-        end;
-    end;
-     Index{i} = TempIndex;
-end;
-
-% replace the parameters in the formula starting with the shortest 
-
-RevisedFormula = Formula;
-
-for i = 1:NumberParams
-    
-    for k = 1:Number(i)
-        
-        % declare temporary
-        NewFormula = '';
-        Remainder = '';
-        
-        % get current length of formula
-        CurrentLength = length(RevisedFormula);
-    
-        % copy from beginning of formula to point before parameter
-        for j = 1:Index{i}(k)-1
-            NewFormula(j) = RevisedFormula(j);
-        end;
-    
-        % add the new parameter name
-        NewFormula = strcat(NewFormula, char(OrderedReplacements{i}));
-    
-        % determine the limit of the remaining formula
-        Limit = CurrentLength - NoCharsInParam(i)-Index{i}(k) + 1;
-        
-        % write formula after occurence of parameter and add to new formula
-        for j = 1:Limit
-            OldIndex = j + Index{i}(k) + NoCharsInParam(i) - 1;
-            Remainder(j) = RevisedFormula(OldIndex);
-        end;
-        
-        NewFormula = strcat(NewFormula, Remainder);
-        
-        % update index
-        % length of formula has changed so locations have changed
-        for j = i:NumberParams
-            for s = 1:length(Index{j})
-                if (Index{j}(s) > Index{i}(k))
-                    Index{j}(s) = Index{j}(s) + length(OrderedReplacements{i}) - NoCharsInParam(i);
-                end;
-            end;
-        end;
-        
-        RevisedFormula = '';
-        RevisedFormula = NewFormula;
-    end;
-    
+RevisedFormula = regexprep(Formula, OrderedCharArray{NumberParams}, OrderedReplacements{NumberParams});
+for i = NumberParams-1:-1:1
+    RevisedFormula = regexprep(RevisedFormula, OrderedCharArray{i}, OrderedReplacements{i});
 end;
 
 y = RevisedFormula;
