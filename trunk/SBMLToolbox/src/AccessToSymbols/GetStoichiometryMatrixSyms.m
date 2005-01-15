e6a46bd18ed72a879fc90040c976bfa1a0325288
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2005-01-15 20:45:41 +0000

    updated for new Determine species role in reactions function

--- trunk/SBMLToolbox/src/AccessToSymbols/GetStoichiometryMatrixSyms.m
+++ trunk/SBMLToolbox/src/AccessToSymbols/GetStoichiometryMatrixSyms.m
@@ -1,5 +1,11 @@
 function varargout = GetStoichiometryMatrixSyms(SBMLModel)
-%
+% GetStoichiometryMatrixSyms(SBMLModel) takes an SBML model 
+% returns 
+%       stoichiometry matrix as first output
+%       array of symbols respresenting each species in same order
+%           as the stoichiometry matrix treats them
+%--------------------------------------------------------------------------
+
 %  Filename    : GetStoichiometryMatrixSyms.m
 %  Description : takes an SBML model and returns the stoichiometry matrix
 %                   and an array of the symbolic represntation of the
@@ -58,76 +64,7 @@ function varargout = GetStoichiometryMatrixSyms(SBMLModel)
 %           as the stoichiometry matrix treats them
 %--------------------------------------------------------------------------
 
-% check input is an SBML model
-if (~isSBML_Model(SBMLModel))
-    error('GetStoichiometryMatrixSyms(SBMLModel)\n%s', 'input must be an SBMLModel structure');
-end;
 
-%------------------------------------------------------------
-% determine the number of species and reactions
-NumSpecies = length(SBMLModel.species);
-NumReactions = length(SBMLModel.reaction);
 
-%--------------------------------------------------------------------------
-% for each species loop through each reaction and determine whether the species
-% takes part and in what capacity
-
-for i = 1:NumSpecies
-
-    %determine which reactions it occurs within 
-    for j = 1:NumReactions
-        
-        SpeciesType = IsSpeciesInReaction(SBMLModel.species(i), SBMLModel.reaction(j));
-       
-        if (SpeciesType(1) == 0)
-            % not in this reaction
-            StoichiometryMatrix(i,j) = 0;
-        else
-
-        % record numbers of occurences of species as reactant/product 
-        % and check that we can deal with reaction
-            
-            NoModifiers = length(SpeciesType);
-            NoReactants = SpeciesType(2);
-            NoProducts =  SpeciesType(3+NoReactants);
-            SavedNoReactants = SpeciesType(2);
-            
-            %--------------------------------------------------------------
-            % check that a species does not occur twice on one side of the
-            % reaction
-            if (NoReactants > 1 || NoProducts > 1)
-                error('GetStoichiometryMatrixSyms(SBMLModel)\n%s', 'SPECIES OCCURS MORE THAN ONCE ON ONE SIDE OF REACTION');
-            end;
-       
-            %--------------------------------------------------------------
-            % check if species is a modifier and exit if it is
-            if (SpeciesType(NoModifiers) > 0)
-                error('GetStoichiometryMatrixSyms(SBMLModel)\n%s', 'CANNOT DEAL WITH MODIFIERS YET');
-            end;
-            %--------------------------------------------------------------
-            
-        end;
-       
-        % species has been found in this reaction
-        while (SpeciesType(1) > 0) % 
-                    
-            if(NoProducts > 0) 
-                StoichiometryMatrix(i,j) = SpeciesType(3+SavedNoReactants+1);
-                NoProducts = NoProducts - 1;
-            elseif (NoReactants > 0) 
-                StoichiometryMatrix(i,j) =  - SpeciesType(3);
-                NoReactants = NoReactants - 1;
-            end; 
-            SpeciesType(1) = SpeciesType(1) - 1;
-            
-        end; % while found > 0
-        
-    end; % for NumReactions
-    
-end; % for NumSpecies
-
-%--------------------------------------------------------------------------
-% assign outputs
-
-varargout{1} = StoichiometryMatrix;
+varargout{1} = GetStoichiometryMatrix(SBMLModel);
 varargout{2} = GetSpeciesSymbols(SBMLModel);
