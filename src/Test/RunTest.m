2df25f34335b8389de0cdc7e78783f60303cfc17
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2005-11-21 11:29:53 +0000

    test script to run all tests

---
+++ src/Test/RunTest.m
@@ -0,0 +1,32 @@
+function RunTest
+
+cd ..;
+cd AccessModel;
+cd Test;
+disp('Performing tests in AccessModel directory');
+RunTest;
+cd ..;
+
+cd ..;
+cd AccessToSymbols;
+cd Test;
+disp('Performing tests in AccessToSymbols directory');
+RunTest;
+cd ..;
+
+cd ..;
+cd Convenience;
+cd Test;
+disp('Performing tests in Convenience directory');
+RunTest;
+cd ..;
+
+cd ..;
+cd Simulation;
+cd Test;
+disp('Performing tests in Simulation directory');
+RunTest;
+cd ..;
+
+cd ..;
+cd Test;
