a1d965e2a3aec7d1873ae0287d5bca1399d6d0f4
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2007-10-26 18:41:37 +0000

    added new l2v2 l2v3 components

---
+++ libSBML API/InitialAssignment/Contents.m
@@ -0,0 +1,10 @@
+% toolbox\MATLAB_SBML_Structure_Functions\InitialAssignment
+%
+% Files
+%   InitialAssignment_create          - InitialAssignment_create 
+%   InitialAssignment_getMath         - InitialAssignment_getMath 
+%   InitialAssignment_getSBOTerm      - InitialAssignment_getSBOTerm 
+%   InitialAssignment_getSymbol       - InitialAssignment_getSymbo
+%   InitialAssignment_setMath         - InitialAssignment_setMath 
+%   InitialAssignment_setSBOTerm      - InitialAssignment_setSBOTerm 
+%   InitialAssignment_setSymbol       - InitialAssignment_setSymbol 
