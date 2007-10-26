f898b45d9cfd347f6f412a6790a2b1bfaebf57e1
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2007-10-26 18:41:37 +0000

    added new l2v2 l2v3 components

---
+++ trunk/SBMLToolbox/libSBML API/CompartmentType/Contents.m
@@ -0,0 +1,10 @@
+% toolbox\MATLAB_SBML_Structure_Functions\COMPARTMENTTYPE
+%
+% Files
+%   CompartmentType_create               - CompartmentType_create 
+%   CompartmentType_getId                - CompartmentType_getId 
+%   CompartmentType_getName              - CompartmentType_getName 
+%   CompartmentType_getSBOTerm           - CompartmentType_getSBOTerm 
+%   CompartmentType_setId                - CompartmentType_setId 
+%   CompartmentType_setName              - CompartmentType_setName 
+%   CompartmentType_setSBOTerm           - CompartmentType_setSBOTerm 
