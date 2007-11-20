54f5bb7af9d69eeef45c908636edb895ebbb63e5
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2007-11-20 23:14:35 +0000

    These functions have been moved to the src/MATLAB_SBML_Structure_Functions directory from the libSBML_API directory. Originally I kept these separate as I was not sure whether they would be part of the toolbox. However they were released and so I'm moving them to make the cvs directory structure match the release directory structure.

--- trunk/SBMLToolbox/libSBML API/CompartmentVolumeRule/Contents.m
+++
@@ -1,7 +0,0 @@
-% toolbox\MATLAB_SBML_Structure_Functions\COMPARTMENTVOLUMERULE
-%
-% Files
-%   CompartmentVolumeRule_create           - CompartmentVolumeRule_create 
-%   CompartmentVolumeRule_getCompartment   - CompartmentVolumeRule_getCompartment 
-%   CompartmentVolumeRule_isSetCompartment - CompartmentVolumeRule_isSetCompartment 
-%   CompartmentVolumeRule_setCompartment   - CompartmentVolumeRule_setCompartment 
