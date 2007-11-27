69b2c3f2cf33f1d3a15433f4bff85c3581a6a149
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2007-11-27 00:07:04 +0000

    tidying up the test files to avoid duplications etc

--- trunk/SBMLToolbox/src/Test/RunTest.m
+++ trunk/SBMLToolbox/src/Test/RunTest.m
@@ -7,12 +7,16 @@ disp('Performing tests in AccessModel directory');
 RunTest;
 cd ..;
 
-cd ..;
-cd AccessToSymbols;
-cd Test;
-disp('Performing tests in AccessToSymbols directory');
-RunTest;
-cd ..;
+v = ver('symbolic');
+% only run if symbolic toolbox is intalled
+if (~isempty(v))
+  cd ..;
+  cd AccessToSymbols;
+  cd Test;
+  disp('Performing tests in AccessToSymbols directory');
+  RunTest;
+  cd ..;
+end;
 
 cd ..;
 cd Convenience;
@@ -22,9 +26,23 @@ RunTest;
 cd ..;
 
 cd ..;
+cd MATLAB_SBML_Structure_Functions;
+cd Test;
+disp('Performing tests in MATLAB_SBML_Structure_Functions directory');
+TestCreate;
+cd ..;
+
+cd ..;
 cd Simulation;
 cd Test;
 disp('Performing tests in Simulation directory');
+RunTest;
+cd ..;
+
+cd ..;
+cd cd Validate_MATLAB_SBML_Structures;
+cd Test;
+disp('Performing tests in cd Validate_MATLAB_SBML_Structures directory');
 RunTest;
 cd ..;
 
