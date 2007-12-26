e2b7a91e9bda9cf3736a3dbdf50b986df8e9d3db
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2007-12-26 20:07:21 +0000

    added functions for creating these new structures

---
+++ trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/Delay/Contents.m
@@ -0,0 +1,8 @@
+% toolbox\MATLAB_SBML_Structure_Functions\Delay
+%
+% Files
+%   Delay_create          - Delay_create 
+%   Delay_getMath         - Delay_getMath 
+%   Delay_getSBOTerm      - Delay_getSBOTerm 
+%   Delay_setMath         - Delay_setMath 
+%   Delay_setSBOTerm      - Delay_setSBOTerm 
