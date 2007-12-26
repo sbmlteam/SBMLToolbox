8d80357d1ef728bc2ba84233d30c522e94ac9261
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2007-12-26 20:07:21 +0000

    added functions for creating these new structures

---
+++ src/MATLAB_SBML_Structure_Functions/StoichiometryMath/Contents.m
@@ -0,0 +1,8 @@
+% toolbox\MATLAB_SBML_Structure_Functions\StoichiometryMath
+%
+% Files
+%   StoichiometryMath_create          - StoichiometryMath_create 
+%   StoichiometryMath_getMath         - StoichiometryMath_getMath 
+%   StoichiometryMath_getSBOTerm      - StoichiometryMath_getSBOTerm 
+%   StoichiometryMath_setMath         - StoichiometryMath_setMath 
+%   StoichiometryMath_setSBOTerm      - StoichiometryMath_setSBOTerm 
---
+++ src/MATLAB_SBML_Structure_Functions/Trigger/Contents.m
@@ -0,0 +1,8 @@
+% toolbox\MATLAB_SBML_Structure_Functions\Trigger
+%
+% Files
+%   Trigger_create          - Trigger_create 
+%   Trigger_getMath         - Trigger_getMath 
+%   Trigger_getSBOTerm      - Trigger_getSBOTerm 
+%   Trigger_setMath         - Trigger_setMath 
+%   Trigger_setSBOTerm      - Trigger_setSBOTerm 
