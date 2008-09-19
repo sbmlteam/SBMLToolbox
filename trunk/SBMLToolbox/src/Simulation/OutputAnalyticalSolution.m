917e6ea80f9449b429c342b9112442bc4316c6af
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2008-09-19 21:05:42 +0000

    added functions to calculate an analytical solution

---
+++ trunk/SBMLToolbox/src/Simulation/OutputAnalyticalSolution.m
@@ -0,0 +1,206 @@
+function OutputAnalyticalSolution(varargin)
+% OutputODEFunction takes 
+%       1) a MATLAB_SBML model structure 
+%       2) a flag to indicate whether to plot the output (optional)
+%       3) time limit (optional)
+%       4) number of time steps (optional)
+%       5) a flag to indicate whther to output the simulation data as 
+%           a comma separated variable (csv) file
+
+%  Filename    :   OutputAnalyticalSolution.m
+%  Description :
+%  Author(s)   :   SBML Development Group <sbml-team@caltech.edu>
+%  $Id: OutputODEFunction.m 8181 2008-09-18 17:43:37Z sarahkeating $
+%  $Source v $
+%
+%<!---------------------------------------------------------------------------
+% This file is part of SBMLToolbox.  Please visit http://sbml.org for more
+% information about SBML, and the latest version of SBMLToolbox.
+%
+% Copyright 2005-2007 California Institute of Technology.
+% Copyright 2002-2005 California Institute of Technology and
+%                     Japan Science and Technology Corporation.
+% 
+% This library is free software; you can redistribute it and/or modify it
+% under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation.  A copy of the license agreement is provided
+% in the file named "LICENSE.txt" included with this software distribution.
+% and also available online as http://sbml.org/software/sbmltoolbox/license.html
+%----------------------------------------------------------------------- -->
+% get inputs
+if (nargin < 1)
+    error('OutputAnalyticalSolution(SBMLModel, ...)\n%s', 'must have at least one arguments');
+elseif (nargin > 5)
+    error('OutputAnalyticalSolution(SBMLModel, ...)\n%s', 'cannot have more than five arguments');
+end;
+
+
+SBMLModel = varargin{1};
+
+% check first input is an SBML model
+if (~isSBML_Model(SBMLModel))
+    error('OutputAnalyticalSolution(SBMLModel)\n%s', 'first argument must be an SBMLModel structure');
+end;
+
+soln = SolveAnalytically(SBMLModel);
+
+if (~isstruct(soln))
+  error('Error producing analytical solution');
+end;
+%------------------------------------------------------------
+% calculate values to use in iterative process
+if (nargin > 2)
+    Time_limit = varargin{3};
+else
+    Time_limit = 10;
+end;
+
+if (nargin > 3)
+    delta_t = Time_limit/varargin{4};
+    Time_span = [0:delta_t:Time_limit];
+    Number_points = length(Time_span);
+else
+    Time_span = [0, Time_limit];
+    delta_t = 1;
+    Number_points = length(Time_span);
+end;
+
+% check third argument
+if ((length(Time_limit) ~= 1) || (~isnumeric(Time_limit)))
+    error('OutputAnalyticalSolution(SBMLModel, time)\n%s', 'third argument must be a single real number indicating a time limit');
+end;
+
+plotOutput = 0;
+if (nargin > 1)
+  plotOutput = varargin{2};
+end;
+
+csvOutput = 0;
+if (nargin > 4)
+  csvOutput = varargin{5};
+end;
+
+ff = fieldnames(soln);
+number = length(ff);
+
+index = 1;
+for i=0:delta_t:Time_limit
+  TimeCourse(index) = i;  
+  v = subs(soln.(ff{1}), 't', i);
+  if (isnumeric(v))
+    SpeciesCourse(1, index) = v;
+  else
+    disp(strcat(filename, ' encountered a symbolic expression'));
+    i=time;
+    break;
+  end;
+  for j=2:number
+    v = subs(soln.(ff{j}), 't', i);
+    if (isnumeric(v))
+      SpeciesCourse(j, index) = v;
+    else
+      disp('Encountered a symbolic expression');
+      i=time;
+      j=number;
+      break;
+    end;
+  end;
+  index = index + 1;
+end;
+
+Species = GetSpecies(SBMLModel);
+
+%--------------------------------------------------------------
+if (plotOutput == 1)
+    if (~(ismember(1, isnan(SpeciesCourse))))
+
+        PlotSpecies = SelectSpecies(SBMLModel);
+
+        % line styles - will look at these
+        Types = {'-oc', '-om', '-oy', '-ok', '-or', '-og', '-ob', '--c', '--m', '--y', '--k', '--r', '--g', '--b'};
+        j = 1;
+
+        %plot the output
+        plotCount = 1;
+        for i = 1:length(Species)
+            % check whether to plot
+            Plot = 0;
+            for k = 1:length(PlotSpecies)
+                if (strcmp(PlotSpecies{k}, Species{i}))
+                    Plot = 1;
+                end;
+            end;
+
+            if (Plot == 1)
+                if (j > 14)
+                    j = 1;
+                end;
+                plot(TimeCourse,SpeciesCourse(i, :),Types{j});
+                hold on;
+                j = j+1;
+                PlottedSpecies{plotCount} = Species{i};
+                plotCount = plotCount + 1;
+            end;
+        end;
+        if (Plot ~= 0)
+            hold off;
+            xlabel('time t');
+            ylabel('amount');
+            legend(PlottedSpecies, -1);
+        end;
+    end;
+end;
+%-------------------------------------------------------------------------
+% write output file
+% if simulation has failed do not
+if (~(ismember(1, isnan(SpeciesCourse))))
+    if (csvOutput == 1)
+        %------------------------------------------------------------
+
+        % get the name/id of the model
+
+        Name = '';
+        if (SBMLModel.SBML_level == 1)
+            Name = SBMLModel.name;
+        else
+            if (isempty(SBMLModel.id))
+                Name = SBMLModel.name;
+            else
+                Name = SBMLModel.id;
+            end;
+        end;
+
+        if (length(Name) > 63)
+            Name = Name(1:60);
+        end;
+        fileName = strcat(Name, '.csv');
+      
+        %--------------------------------------------------------------------
+        % open the file for writing
+
+        fileID = fopen(fileName, 'w');
+
+        % write the header
+        fprintf(fileID,  'time');
+        for i = 1: length(Species)
+            fprintf(fileID, ',%s', Species{i});
+        end;
+        fprintf(fileID, '\n');
+
+        % write each time course step values
+        for i = 1:length(TimeCourse)
+            fprintf(fileID, '%0.5g', TimeCourse(i));
+
+            for j = 1:length(Species)
+                fprintf(fileID, ',%1.16g', SpeciesCourse(j, i));
+            end;
+            fprintf(fileID, '\n');
+
+        end;
+
+        fclose(fileID);
+
+
+    end;
+end;
+
---
+++ trunk/SBMLToolbox/src/Simulation/SolveAnalytically.m
@@ -0,0 +1,121 @@
+function y = SolveAnalytically(SBMLModel)
+% SolveAnalytically takes an SBMLModel 
+% and returns 
+%           1) a structure detailing each species and the resulting anaytical solution 
+%
+% NOTE: This function requires the MATLAB Symbolic Toolbox
+%--------------------------------------------------------------------------
+
+%
+%  Filename    : SolveAnalytically.m
+%  Description : % SolveAnalytically takes a SBMLModel 
+% and returns   1) an array of symbols representing all parameters (both global and embedded) within the model 
+%               2) an array of the values of each parameter
+%               3) an array of character names of the symbols
+%  Author(s)   : SBML Development Group <sbml-team@caltech.edu>
+%  $Id: ASTNode.h 8088 2008-08-21 14:32:27Z mhucka $
+%  $HeadURL: https://sbml.svn.sourceforge.net/svnroot/sbml/trunk/libsbml/src/math/ASTNode.h $
+%
+%<!---------------------------------------------------------------------------
+% This file is part of SBMLToolbox.  Please visit http://sbml.org for more
+% information about SBML, and the latest version of SBMLToolbox.
+%
+% Copyright 2005-2007 California Institute of Technology.
+% Copyright 2002-2005 California Institute of Technology and
+%                     Japan Science and Technology Corporation.
+% 
+% This library is free software; you can redistribute it and/or modify it
+% under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation.  A copy of the license agreement is provided
+% in the file named "LICENSE.txt" included with this software distribution.
+% and also available online as http://sbml.org/software/sbmltoolbox/license.html
+%----------------------------------------------------------------------- -->
+
+% catch errors in input
+symbolicToolboxPresent = false;
+toolboxes = ver;
+for i=1:length(toolboxes)
+  if (strcmp(toolboxes(i).Name, 'Symbolic Math Toolbox'))
+    symbolicToolboxPresent = true;
+  end;
+end;
+if (~symbolicToolboxPresent)
+  error('SolveAnalytically(SBMLModel) requires the Symbolic Math Toolbox');
+end;
+% check first input is an SBML model
+if (~isSBML_Model(SBMLModel))
+    error('SolveAnalytically(SBMLModel)\n%s', 'first argument must be an SBMLModel structure');
+end;
+
+
+Species = AnalyseSpecies(SBMLModel);
+
+% write out species changes
+for i=1:length(Species)
+  if (Species(i).ChangedByReaction == 1)
+    for fd = 1:Model_getNumFunctionDefinitions(SBMLModel)
+      newFormula = SubstituteFunction(Species(i).KineticLaw{1}, Model_getFunctionDefinition(SBMLModel, fd));
+      if (~isempty(newFormula))
+        Species(i).KineticLaw{1} = newFormula;
+      end;
+    end;
+    eqn{i} = sprintf('D%s = %s', Species(i).Name{1}, Species(i).KineticLaw{1});
+  elseif (Species(i).ChangedByRateRule == 1)
+    for fd = 1:Model_getNumFunctionDefinitions(SBMLModel)
+      newFormula = SubstituteFunction(Species(i).RateRule{1}, Model_getFunctionDefinition(SBMLModel, fd));
+      if (~isempty(newFormula))
+        Species(i).RateRule{1} = newFormula;
+      end;
+    end;
+    eqn{i} = sprintf('D%s = %s', Species(i).Name{1}, Species(i).RateRule{1});
+  elseif (Species(i).ChangedByAssignmentRule == 1)
+    for fd = 1:Model_getNumFunctionDefinitions(SBMLModel)
+      newFormula = SubstituteFunction(Species(i).AssignmentRule{1}, Model_getFunctionDefinition(SBMLModel, fd));
+      if (~isempty(newFormula))
+        Species(i).AssignmentRule{1} = newFormula;
+      end;
+    end;
+    eqn{i} = sprintf('D%s = %s', Species(i).Name{1}, Differentiate(SBMLModel, Species(i).AssignmentRule{1}));
+  elseif (Species(i).ConvertedToAssignRule == 1)
+    for fd = 1:Model_getNumFunctionDefinitions(SBMLModel)
+      newFormula = SubstituteFunction(Species(i).ConvertedRule{1}, Model_getFunctionDefinition(SBMLModel, fd));
+      if (~isempty(newFormula))
+        Species(i).ConvertedRule{1} = newFormula;
+      end;
+    end;
+    eqn{i} = sprintf('D%s = %s', Species(i).Name{1}, Differentiate(SBMLModel, Species(i).ConvertedRule{1}));
+  else
+    eqn{i} = sprintf('D%s = 0', Species(i).Name{1});
+  end;
+  eqn{i} = SubstituteConstants(eqn{i}, SBMLModel);
+  val{i} = sprintf('%s(0) = %g', Species(i).Name{1}, Species(i).initialValue);
+end;
+
+y = dsolve(eqn{1:end}, val{1:end});
+
+
+function newRule = Differentiate(m, rule)
+
+species = GetSpecies(m);
+
+r = rule;
+for i=1:length(species)
+  r = strrep(r, species(i), strcat(species(i), '(t)'));
+end;
+
+for i=1:length(m.compartment)
+  if (m.compartment(i).constant ~= 1)
+    r = strrep(r, m.compartment(i).id, strcat(m.compartment(i).id, '(t)'));
+  end;
+end;
+
+for i=1:length(m.parameter)
+  if (m.parameter(i).constant ~= 1)
+    r = strrep(r, m.parameter(i).id, strcat(m.parameter(i).id, '(t)'));
+  end;
+end;
+
+syms t;
+symr = sym(r{1});
+newsymr = diff(symr, t);
+newRule = char(newsymr);
