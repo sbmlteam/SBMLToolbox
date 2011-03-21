800456b761cf1f03ddf288fee0d8813fb3c4d656
mhucka <mhucka@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-03-21 05:40:36 +0000

    Updated copyrights.

--- src/make.bat
+++ src/make.bat
@@ -10,15 +10,23 @@ rem<!---------------------------------------------------------------------------
 rem This file is part of SBMLToolbox.  Please visit http://sbml.org for more
 rem information about SBML, and the latest version of SBMLToolbox.
 rem
-rem Copyright 2005-2007 California Institute of Technology.
-rem Copyright 2002-2005 California Institute of Technology and
-rem                     Japan Science and Technology Corporation.
-rem 
-rem This library is free software; you can redistribute it and/or modify it
+rem Copyright (C) 2009-2011 jointly by the following organizations: 
+rem     1. California Institute of Technology, Pasadena, CA, USA
+rem     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+rem
+rem Copyright (C) 2006-2008 jointly by the following organizations: 
+rem     1. California Institute of Technology, Pasadena, CA, USA
+rem     2. University of Hertfordshire, Hatfield, UK
+rem
+rem Copyright (C) 2003-2005 jointly by the following organizations: 
+rem     1. California Institute of Technology, Pasadena, CA, USA 
+rem     2. Japan Science and Technology Agency, Japan
+rem     3. University of Hertfordshire, Hatfield, UK
+rem
+rem SBMLToolbox is free software; you can redistribute it and/or modify it
 rem under the terms of the GNU Lesser General Public License as published by
 rem the Free Software Foundation.  A copy of the license agreement is provided
-rem in the file named "LICENSE.txt" included with this software distribution
-rem and also available online as http://sbml.org/software/sbmltoolbox/license.html
+rem in the file named "LICENSE.txt" included with this software distribution.
 rem----------------------------------------------------------------------- -->*/
 
 matlab -r install
