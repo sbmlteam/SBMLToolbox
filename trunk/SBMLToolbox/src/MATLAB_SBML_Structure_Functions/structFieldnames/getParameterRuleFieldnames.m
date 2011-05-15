08cbda9401b2faf210b80a96ed7f4f136e9720b8
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-05-15 10:32:02 +0000

    renamed fieldnames to structFieldnames since fieldnames is a function in matlab

---
+++ trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/structFieldnames/getParameterRuleFieldnames.m
@@ -0,0 +1,80 @@
+function [SBMLfieldnames, nNumberFields] = getParameterRuleFieldnames(level, ...
+                                                             version)
+
+%
+% getParameterRuleFieldnames
+%    takes an SBML level
+%     and  an SBML version
+%
+%    returns
+%      1) an array fieldnames for the ParameterRule structure of the appropriate
+%                  level and version
+%      2) the number of fieldnames expected
+
+%  Filename    :   getParameterRuleFieldnames.m
+%  Description :
+%  Author(s)   :   SBML Development Group <sbml-team@caltech.edu>
+%  $Id: $
+%  $Source v $
+%
+%<!---------------------------------------------------------------------------
+% This file is part of SBMLToolbox.  Please visit http://sbml.org for more
+% information about SBML, and the latest version of SBMLToolbox.
+%
+% Copyright (C) 2009-2011 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA
+%     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+%
+% Copyright (C) 2006-2008 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA
+%     2. University of Hertfordshire, Hatfield, UK
+%
+% Copyright (C) 2003-2005 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA 
+%     2. Japan Science and Technology Agency, Japan
+%     3. University of Hertfordshire, Hatfield, UK
+%
+% SBMLToolbox is free software; you can redistribute it and/or modify it
+% under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation.  A copy of the license agreement is provided
+% in the file named "LICENSE.txt" included with this software distribution.
+%----------------------------------------------------------------------- -->
+
+
+if (~isValidLevelVersionCombination(level, version))
+  error ('invalid level/version combination');
+end;
+
+if (level == 1)
+		SBMLfieldnames = { 'typecode', ...
+		                   'notes', ...
+		                   'annotation', ...
+		                   'type', ...
+		                   'formula', ...
+		                   'variable', ...
+		                   'species', ...
+		                   'compartment', ...
+		                   'name', ...
+		                   'units', ...
+		                 };
+		nNumberFields = 10;
+elseif (level == 2)
+	if (version == 1)
+		SBMLfieldnames = [];
+		nNumberFields = 0;
+	elseif (version == 2)
+		SBMLfieldnames = [];
+		nNumberFields = 0;
+	elseif (version == 3)
+		SBMLfieldnames = [];
+		nNumberFields = 0;
+	elseif (version == 4)
+		SBMLfieldnames = [];
+		nNumberFields = 0;
+	end;
+elseif (level == 3)
+	if (version == 1)
+		SBMLfieldnames = [];
+		nNumberFields = 0;
+	end;
+end;
