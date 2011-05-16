bb02e1603a55356b45668dea31f75f0ed393619e
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-05-16 13:25:56 +0000

    new manipulation functions

---
+++ trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/AlgebraicRule/AlgebraicRule_setSBOTerm.m
@@ -0,0 +1,53 @@
+function SBMLAlgebraicRule = AlgebraicRule_setSBOTerm(SBMLAlgebraicRule, sboTerm)
+%
+% AlgebraicRule_setSBOTerm
+%    takes an SBML AlgebraicRule structure
+%    and the sboTerm to be set
+%
+%    returns
+%      the AlgebraicRule with the new value for the sboTerm attribute
+
+%  Filename    :   AlgebraicRule_setSBOTerm.m
+%  Description :
+%  Author(s)   :   SBML Development Group <sbml-team@caltech.edu>
+%  $Id: $
+%  $Source v $
+%
+%<!---------------------------------------------------------------------------
+% This file is part of SBMLToolbox.  Please visit http://sbml.org for more
+% information about SBML, and the latest version of SBMLToolbox.
+%
+% Copyright (C) 2009-2011 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA
+%     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+%
+% Copyright (C) 2006-2008 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA
+%     2. University of Hertfordshire, Hatfield, UK
+%
+% Copyright (C) 2003-2005 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA 
+%     2. Japan Science and Technology Agency, Japan
+%     3. University of Hertfordshire, Hatfield, UK
+%
+% SBMLToolbox is free software; you can redistribute it and/or modify it
+% under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation.  A copy of the license agreement is provided
+% in the file named "LICENSE.txt" included with this software distribution.
+%----------------------------------------------------------------------- -->
+
+
+%get level and version and check the input arguments are appropriate
+
+[level, version] = GetLevelVersion(SBMLAlgebraicRule);
+
+if isfield(SBMLAlgebraicRule, 'sboTerm')
+	if ~isIntegralNumber(sboTerm)
+		error('sboTerm must be an integer') ;
+	else
+		SBMLAlgebraicRule.sboTerm = sboTerm;
+	end;
+else
+	error('sboTerm not an attribute on SBML L%dV%d AlgebraicRule', level, version);
+end;
+
---
+++ trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/AlgebraicRule/AlgebraicRule_unsetMetaid.m
@@ -0,0 +1,48 @@
+function SBMLAlgebraicRule = AlgebraicRule_unsetMetaid(SBMLAlgebraicRule)
+%
+% AlgebraicRule_getMetaid
+%    takes an SBML AlgebraicRule structure
+%
+%    returns
+%      the AlgebraicRule with the value for the metaid attribute unset
+
+%  Filename    :   AlgebraicRule_unsetMetaid.m
+%  Description :
+%  Author(s)   :   SBML Development Group <sbml-team@caltech.edu>
+%  $Id: $
+%  $Source v $
+%
+%<!---------------------------------------------------------------------------
+% This file is part of SBMLToolbox.  Please visit http://sbml.org for more
+% information about SBML, and the latest version of SBMLToolbox.
+%
+% Copyright (C) 2009-2011 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA
+%     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+%
+% Copyright (C) 2006-2008 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA
+%     2. University of Hertfordshire, Hatfield, UK
+%
+% Copyright (C) 2003-2005 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA 
+%     2. Japan Science and Technology Agency, Japan
+%     3. University of Hertfordshire, Hatfield, UK
+%
+% SBMLToolbox is free software; you can redistribute it and/or modify it
+% under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation.  A copy of the license agreement is provided
+% in the file named "LICENSE.txt" included with this software distribution.
+%----------------------------------------------------------------------- -->
+
+
+%get level and version and check the input arguments are appropriate
+
+[level, version] = GetLevelVersion(SBMLAlgebraicRule);
+
+if isfield(SBMLAlgebraicRule, 'metaid')
+	SBMLAlgebraicRule.metaid = '';
+else
+	error('metaid not an attribute on SBML L%dV%d AlgebraicRule', level, version);
+end;
+
