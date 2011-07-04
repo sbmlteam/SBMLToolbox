3307535808f7f590fde6c3833258fd1bcc6c672d
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-07-04 09:47:05 +0000

    updated documentation

--- src/MATLAB_SBML_Structure_Functions/EventAssignment/EventAssignment_isSetMetaid.m
+++ src/MATLAB_SBML_Structure_Functions/EventAssignment/EventAssignment_isSetMetaid.m
@@ -1,18 +1,17 @@
 function value = EventAssignment_isSetMetaid(SBMLEventAssignment)
+% value = EventAssignment_isSetMetaid(SBMLEventAssignment)
 %
-% EventAssignment_isSetMetaid
-%       takes an SBML EventAssignment structure
+% takes
 %
-%       and returns
-%           1 if the value for the metaid attribute is set
-%           0 otherwise
+% 1. SBMLEventAssignment; an SBML EventAssignment structure
+%
+% returns
+%
+% 1. value = 
+%  - 1 if the metaid attribute is set
+%  - 0 otherwise
+%
 
-%  Filename    :   EventAssignment_isSetMetaid.m
-%  Description :
-%  Author(s)   :   SBML Development Group <sbml-team@caltech.edu>
-%  $Id: $
-%  $Source v $
-%
 %<!---------------------------------------------------------------------------
 % This file is part of SBMLToolbox.  Please visit http://sbml.org for more
 % information about SBML, and the latest version of SBMLToolbox.
@@ -35,6 +34,8 @@ function value = EventAssignment_isSetMetaid(SBMLEventAssignment)
 % the Free Software Foundation.  A copy of the license agreement is provided
 % in the file named "LICENSE.txt" included with this software distribution.
 %----------------------------------------------------------------------- -->
+
+
 
 
 %get level and version and check the input arguments are appropriate
