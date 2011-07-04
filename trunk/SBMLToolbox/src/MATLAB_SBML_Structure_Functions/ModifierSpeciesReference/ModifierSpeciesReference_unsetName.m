218d7d483feae9dc71b7e42c43ea6530fc2e3c22
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-07-04 10:10:32 +0000

    updated documentation

--- trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/ModifierSpeciesReference/ModifierSpeciesReference_unsetName.m
+++ trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/ModifierSpeciesReference/ModifierSpeciesReference_unsetName.m
@@ -1,17 +1,15 @@
 function SBMLModifierSpeciesReference = ModifierSpeciesReference_unsetName(SBMLModifierSpeciesReference)
+% SBMLModifierSpeciesReference = ModifierSpeciesReference_unsetName(SBMLModifierSpeciesReference)
 %
-% ModifierSpeciesReference_getName
-%       takes an SBML ModifierSpeciesReference structure
+% takes
 %
-%       and returns
-%           the ModifierSpeciesReference with the value for the name attribute unset
+% 1. SBMLModifierSpeciesReference; an SBML ModifierSpeciesReference structure
+%
+% returns
+%
+% 1. the SBML ModifierSpeciesReference structure with the name attribute unset
+%
 
-%  Filename    :   ModifierSpeciesReference_unsetName.m
-%  Description :
-%  Author(s)   :   SBML Development Group <sbml-team@caltech.edu>
-%  $Id: $
-%  $Source v $
-%
 %<!---------------------------------------------------------------------------
 % This file is part of SBMLToolbox.  Please visit http://sbml.org for more
 % information about SBML, and the latest version of SBMLToolbox.
@@ -34,6 +32,8 @@ function SBMLModifierSpeciesReference = ModifierSpeciesReference_unsetName(SBMLM
 % the Free Software Foundation.  A copy of the license agreement is provided
 % in the file named "LICENSE.txt" included with this software distribution.
 %----------------------------------------------------------------------- -->
+
+
 
 
 %get level and version and check the input arguments are appropriate
