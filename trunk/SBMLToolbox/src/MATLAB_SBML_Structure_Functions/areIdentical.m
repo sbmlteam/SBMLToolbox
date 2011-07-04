1159fbd06b4e762085cc504065b08d2ccf0f2395
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-07-04 14:02:39 +0000

    updated documentation

--- trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/areIdentical.m
+++ trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/areIdentical.m
@@ -1,4 +1,39 @@
 function identical = areIdentical(struct1, struct2)
+% identical = areIdentical(SBMLStruct1, SBMLStruct2)
+%
+% takes
+%
+% 1. SBMLStruct1; any SBML structure
+% 2. SBMLStruct2; any SBML structure
+%
+% returns
+%
+% 1. identical = 
+%   - 1 if the structures are identical i.e. contain same fields and the same values
+%   - 0 otherwise
+
+%<!---------------------------------------------------------------------------
+% This file is part of SBMLToolbox.  Please visit http://sbml.org for more
+% information about SBML, and the latest version of SBMLToolbox.
+%
+% Copyright (C) 2009-2011 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA
+%     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+%
+% Copyright (C) 2006-2008 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA
+%     2. University of Hertfordshire, Hatfield, UK
+%
+% Copyright (C) 2003-2005 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA 
+%     2. Japan Science and Technology Agency, Japan
+%     3. University of Hertfordshire, Hatfield, UK
+%
+% SBMLToolbox is free software; you can redistribute it and/or modify it
+% under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation.  A copy of the license agreement is provided
+% in the file named "LICENSE.txt" included with this software distribution.
+%----------------------------------------------------------------------- -->
 
 if ~isValid(struct1)
   error('first argument must be an SBML Structure');
