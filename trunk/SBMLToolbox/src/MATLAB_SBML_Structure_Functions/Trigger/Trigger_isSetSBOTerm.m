bad47b88530be2760a153385549772fcd7e3010f
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-07-04 11:05:39 +0000

    updated documentation

--- trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/Trigger/Trigger_isSetSBOTerm.m
+++ trunk/SBMLToolbox/src/MATLAB_SBML_Structure_Functions/Trigger/Trigger_isSetSBOTerm.m
@@ -1,18 +1,17 @@
 function value = Trigger_isSetSBOTerm(SBMLTrigger)
+% value = Trigger_isSetSBOTerm(SBMLTrigger)
 %
-% Trigger_isSetSBOTerm
-%       takes an SBML Trigger structure
+% takes
 %
-%       and returns
-%           1 if the value for the sboTerm attribute is set
-%           0 otherwise
+% 1. SBMLTrigger; an SBML Trigger structure
+%
+% returns
+%
+% 1. value = 
+%  - 1 if the sboTerm attribute is set
+%  - 0 otherwise
+%
 
-%  Filename    :   Trigger_isSetSBOTerm.m
-%  Description :
-%  Author(s)   :   SBML Development Group <sbml-team@caltech.edu>
-%  $Id: $
-%  $Source v $
-%
 %<!---------------------------------------------------------------------------
 % This file is part of SBMLToolbox.  Please visit http://sbml.org for more
 % information about SBML, and the latest version of SBMLToolbox.
@@ -35,6 +34,8 @@ function value = Trigger_isSetSBOTerm(SBMLTrigger)
 % the Free Software Foundation.  A copy of the license agreement is provided
 % in the file named "LICENSE.txt" included with this software distribution.
 %----------------------------------------------------------------------- -->
+
+
 
 
 %get level and version and check the input arguments are appropriate
