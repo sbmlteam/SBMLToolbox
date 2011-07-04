c1273993321405757a4b259ca301480eb32b04ac
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-07-04 09:53:03 +0000

    updated documentation

--- src/MATLAB_SBML_Structure_Functions/FunctionDefinition/FunctionDefinition_getMath.m
+++ src/MATLAB_SBML_Structure_Functions/FunctionDefinition/FunctionDefinition_getMath.m
@@ -1,17 +1,15 @@
 function math = FunctionDefinition_getMath(SBMLFunctionDefinition)
+% math = FunctionDefinition_getMath(SBMLFunctionDefinition)
 %
-% FunctionDefinition_getMath
-%       takes an SBML FunctionDefinition structure
+% takes
 %
-%       and returns
-%           the value of the math attribute
+% 1. SBMLFunctionDefinition; an SBML FunctionDefinition structure
+%
+% returns
+%
+% 1. the value of the math attribute
+%
 
-%  Filename    :   FunctionDefinition_getMath.m
-%  Description :
-%  Author(s)   :   SBML Development Group <sbml-team@caltech.edu>
-%  $Id$
-%  $Source v $
-%
 %<!---------------------------------------------------------------------------
 % This file is part of SBMLToolbox.  Please visit http://sbml.org for more
 % information about SBML, and the latest version of SBMLToolbox.
@@ -34,6 +32,8 @@ function math = FunctionDefinition_getMath(SBMLFunctionDefinition)
 % the Free Software Foundation.  A copy of the license agreement is provided
 % in the file named "LICENSE.txt" included with this software distribution.
 %----------------------------------------------------------------------- -->
+
+
 
 
 %get level and version and check the input arguments are appropriate
--- src/MATLAB_SBML_Structure_Functions/FunctionDefinition/FunctionDefinition_setId.m
+++ src/MATLAB_SBML_Structure_Functions/FunctionDefinition/FunctionDefinition_setId.m
@@ -1,18 +1,16 @@
 function SBMLFunctionDefinition = FunctionDefinition_setId(SBMLFunctionDefinition, id)
+% SBMLFunctionDefinition = FunctionDefinition_setId(SBMLFunctionDefinition, id)
 %
-% FunctionDefinition_setId
-%       takes an SBML FunctionDefinition structure
-%    and the id to be set
+% takes
 %
-%       and returns
-%           the FunctionDefinition with the new value for the id attribute
+% 1. SBMLFunctionDefinition; an SBML FunctionDefinition structure
+% 2. id; a string representing the id to be set
+%
+% returns
+%
+% 1. the SBML FunctionDefinition structure with the new value for the id attribute
+%
 
-%  Filename    :   FunctionDefinition_setId.m
-%  Description :
-%  Author(s)   :   SBML Development Group <sbml-team@caltech.edu>
-%  $Id$
-%  $Source v $
-%
 %<!---------------------------------------------------------------------------
 % This file is part of SBMLToolbox.  Please visit http://sbml.org for more
 % information about SBML, and the latest version of SBMLToolbox.
@@ -35,6 +33,8 @@ function SBMLFunctionDefinition = FunctionDefinition_setId(SBMLFunctionDefinitio
 % the Free Software Foundation.  A copy of the license agreement is provided
 % in the file named "LICENSE.txt" included with this software distribution.
 %----------------------------------------------------------------------- -->
+
+
 
 
 %get level and version and check the input arguments are appropriate
