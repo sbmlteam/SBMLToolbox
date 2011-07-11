19b9af7b642bbb0eb4d3d2e87479721fab95f607
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-07-11 13:47:00 +0000

    updated to produce current html

--- trunk/SBMLToolbox/dev/utilities/generating_scripts/writeStructDocs.m
+++ trunk/SBMLToolbox/dev/utilities/generating_scripts/writeStructDocs.m
@@ -38,12 +38,13 @@ end;
 if isempty(values)
   values = getDefaultValues(obj, 3, 1);
 end;
-fprintf(fOut, '<p>typecode = %s</p><br/>', values{1});
+%fprintf(fOut, '<p>typecode = %s</p><br/>', values{1});
 
+% For each level
 % create the outer table with the level/version headers
-writeOutertable(fOut);
-
-fprintf(fOut, '\t<tr>\n');
+fprintf(fOut, '\n\n<br/><br/>\n\n');
+writeOutertable(fOut, 1, 1);
+fprintf(fOut, '\t<tr valign=\"top\">\n');
 
 % inside this outer table inside each column write the table for the appropriate level/version 
 fprintf(fOut, '\t\t<td>\n');
@@ -52,18 +53,48 @@ fprintf(fOut, '\t\t</td>\n');
 fprintf(fOut, '\t\t<td>\n');
 writeTable(fOut, obj, 1, 2);
 fprintf(fOut, '\t\t</td>\n');
+
+% close the outer table
+fprintf(fOut, '\t</tr>\n');
+fprintf(fOut, '</table>\n');
+fprintf(fOut, '</center>\n');
+
+fprintf(fOut, '\n\n<br/><br/>\n\n');
+writeOutertable(fOut, 2, 1);
+fprintf(fOut, '\t<tr valign=\"top\">\n');
+
 fprintf(fOut, '\t\t<td>\n');
 writeTable(fOut, obj, 2, 1);
 fprintf(fOut, '\t\t</td>\n');
 fprintf(fOut, '\t\t<td>\n');
 writeTable(fOut, obj, 2, 2);
 fprintf(fOut, '\t\t</td>\n');
+
+% close the outer table
+fprintf(fOut, '\t</tr>\n');
+fprintf(fOut, '</table>\n');
+fprintf(fOut, '</center>\n');
+
+fprintf(fOut, '\n\n<br/><br/>\n\n');
+writeOutertable(fOut, 2, 3);
+fprintf(fOut, '\t<tr valign=\"top\">\n');
+
 fprintf(fOut, '\t\t<td>\n');
 writeTable(fOut, obj, 2, 3);
 fprintf(fOut, '\t\t</td>\n');
 fprintf(fOut, '\t\t<td>\n');
 writeTable(fOut, obj, 2, 4);
 fprintf(fOut, '\t\t</td>\n');
+
+% close the outer table
+fprintf(fOut, '\t</tr>\n');
+fprintf(fOut, '</table>\n');
+fprintf(fOut, '</center>\n');
+
+fprintf(fOut, '\n\n<br/><br/>\n\n');
+writeOutertable(fOut, 3, 1);
+fprintf(fOut, '\t<tr valign=\"top\">\n');
+
 fprintf(fOut, '\t\t<td>\n');
 writeTable(fOut, obj, 3, 1);
 fprintf(fOut, '\t\t</td>\n');
@@ -78,16 +109,26 @@ fclose(fOut);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % write the top of the outer table with level/version headers
-function writeOutertable(fout)
+function writeOutertable(fout, level, ver)
 
-fprintf(fout, '<center>\n\n<table class=\"borderless-table sm-padding\" width=\"90%%\">\n');
-fprintf(fout, '\t<tr class=\"headers2\">\n\t\t<td>SBML Level 1 Version 1</td>\n');
-fprintf(fout, '\t\t<td>SBML Level 1 Version 2</td>\n');
-fprintf(fout, '\t\t<td>SBML Level 2 Version 1</td>\n');
-fprintf(fout, '\t\t<td>SBML Level 2 Version 2</td>\n');
-fprintf(fout, '\t\t<td>SBML Level 2 Version 3</td>\n');
-fprintf(fout, '\t\t<td>SBML Level 2 Version 4</td>\n');
-fprintf(fout, '\t\t<td>SBML Level 3 Version 1</td>\n');
+if level == 1
+  width = 50;
+elseif level == 2
+  width = 50;
+else
+  width = 35;
+end;
+fprintf(fout, '<center>\n\n<table class=\"borderless-table sm-padding\" width=\"%u%%\">\n', width);
+fprintf(fout, '<caption><strong>SBML Level %u</strong></caption>\n', level);
+if (ver == 1)
+  fprintf(fout, '\t<tr class=\"headers2\">\n\t\t<td>Version 1</td>\n');
+  if level < 3
+  fprintf(fout, '\t\t<td>Version 2</td>\n');
+  end;
+else
+  fprintf(fout, '\t<tr class=\"headers2\">\n\t\t<td>Version 3</td>\n');
+  fprintf(fout, '\t\t<td>Version 4</td>\n');
+end;
 fprintf(fout, '\t</tr>\n');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -96,7 +137,7 @@ function writeTable(fout, obj, l, v)
 
 [fieldnames, num] = getFieldnames(obj, l, v);
 if (num == 0)
-  fprintf(fout, 'N/A\n');
+  fprintf(fout, 'The %s component is not part of SBML Level %u Version %u\n', obj, l, v);
   return;
 end;
 values = getDefaultValues(obj, l, v);
@@ -108,10 +149,14 @@ fprintf(fout, '\t\t\t\t\t<tr class=\"headers\">\n');
 fprintf(fout, '\t\t\t\t\t\t<td class=\"struct\">Fieldname</td>\n');
 fprintf(fout, '\t\t\t\t\t\t<td class=\"type\">Type</td>\n');
 fprintf(fout, '\t\t\t\t\t</tr>\n');
-for i = 1:length(fieldnames)
   fprintf(fout, '\t\t\t\t\t<tr>\n');
-  fprintf(fout, '\t\t\t\t\t\t<td> %s </td>\n', fieldnames{i});
-  fprintf(fout, '\t\t\t\t\t\t<td> %s </td>\n', getType(values{i}));
+  fprintf(fout, '\t\t\t\t\t\t<td><code> %s </code></td>\n', fieldnames{1});
+  fprintf(fout, '\t\t\t\t\t\t<td> %s </td>\n', values{1});
+  fprintf(fout, '\t\t\t\t\t</tr>\n');
+for i = 2:length(fieldnames)
+  fprintf(fout, '\t\t\t\t\t<tr>\n');
+  fprintf(fout, '\t\t\t\t\t\t<td><code> %s </code></td>\n', fieldnames{i});
+  fprintf(fout, '\t\t\t\t\t\t<td> %s </td>\n', getType(values{i}, fieldnames{i}, l, v));
   fprintf(fout, '\t\t\t\t\t</tr>\n');
 end;
 fprintf(fout, '\t\t\t\t</table>\n');
@@ -119,8 +164,19 @@ fprintf(fout, '\t\t\t</center>\n');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % produce appropriate type information from the default value
-function type = getType(value)
+function type = getType(value, name, l, v)
 
+singleObj = {'kineticLaw', 'trigger', 'delay', 'priority', 'stoichiometryMath'};
+stringObj = {'trigger', 'delay', 'stoichiometryMath'};
+
+if isIn(singleObj, name)
+  if (l == 2 && v < 3 && isIn(stringObj, name))
+    type = 'string';
+  else
+  type = 'structure';
+  end;
+  return;
+end;
 if ischar(value)
   type = 'string';
 elseif isinteger(value) && value == -1
@@ -137,3 +193,7 @@ else
   type = 'double';
 end;
   
+
+function value = isIn(array, thing)
+
+value = sum(ismember(array, thing));
