cc843b8ab57547f37eb5be6e58238526b48e86ff
sarahkeating <sarahkeating@c05c2498-8750-0410-b44c-b7d1844e3258>
2011-11-15 15:48:47 +0000

    updated doc

--- src/fbc_package/isBindingFbcEnabled.m
+++ src/fbc_package/isBindingFbcEnabled.m
@@ -1,33 +1,42 @@
 function fbcEnabled = isBindingFbcEnabled(varargin)
-% Checks whether the version of libSBML has been built with 
-% the FBC package extension enabled
-
-% Filename    : isBindingFbcEnabled.m
-% Description : check fbc status
-% Author(s)   : SBML Team <sbml-team@caltech.edu>
-% Organization: EMBL-EBI, Caltech
-% Created     : 2011-02-08
+% fbcEnabled = isBindFbcEnabled(path_to_fbc_file(optional))
 %
-% This file is part of libSBML.  Please visit http://sbml.org for more
-% information about SBML, and the latest version of libSBML.
+% Takes
+%
+% 1. path_to_fbc_file, the path to the fbc.xml file (optional)
+%
+% Returns
+%
+% 1. fbcEnabled = 
+%  - 1 if the executables are enabled with fbc support
+%  - 0 otherwise
+%
+% *NOTE:* if no argument is supplied it is assumed that the path is
+%    './test/test-data/fbc.xml'
+%
+
+%<!---------------------------------------------------------------------------
+% This file is part of SBMLToolbox.  Please visit http://sbml.org for more
+% information about SBML, and the latest version of SBMLToolbox.
 %
 % Copyright (C) 2009-2011 jointly by the following organizations: 
 %     1. California Institute of Technology, Pasadena, CA, USA
 %     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
-%  
-% Copyright (C) 2006-2008 by the California Institute of Technology,
-%     Pasadena, CA, USA 
-%  
-% Copyright (C) 2002-2005 jointly by the following organizations: 
+%
+% Copyright (C) 2006-2008 jointly by the following organizations: 
 %     1. California Institute of Technology, Pasadena, CA, USA
+%     2. University of Hertfordshire, Hatfield, UK
+%
+% Copyright (C) 2003-2005 jointly by the following organizations: 
+%     1. California Institute of Technology, Pasadena, CA, USA 
 %     2. Japan Science and Technology Agency, Japan
-% 
-% This library is free software; you can redistribute it and/or modify
-% it under the terms of the GNU Lesser General Public License as
-% published by the Free Software Foundation.  A copy of the license
-% agreement is provided in the file named "LICENSE.txt" included with
-% this software distribution and also available online as
-% http://sbml.org/software/libsbml/license.html
+%     3. University of Hertfordshire, Hatfield, UK
+%
+% SBMLToolbox is free software; you can redistribute it and/or modify it
+% under the terms of the GNU Lesser General Public License as published by
+% the Free Software Foundation.  A copy of the license agreement is provided
+% in the file named "LICENSE.txt" included with this software distribution.
+%----------------------------------------------------------------------- -->
 
 % assume not enabled
 fbcEnabled = 0;
